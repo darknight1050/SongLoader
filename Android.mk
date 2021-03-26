e867e2ff410f53bbe340ba7ed9baf039c7fb54c5
darknight1050 <darkdeveloping1050@gmail.com>
2021-03-26 22:56:30 +0100

    Use exceptions in Android.mk

--- Android.mk
+++ Android.mk
@@ -71,6 +71,7 @@ LOCAL_SHARED_LIBRARIES += codegen_0_7_0
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += cryptopp
+LOCAL_CPP_FEATURES += exceptions
 LOCAL_LDLIBS += -llog 
 LOCAL_CFLAGS += -I"include" -I"shared" -I"./extern/libil2cpp/il2cpp/libil2cpp" -I"extern" -I"extern/codegen/include" -DVERSION='"0.1.0"'
 LOCAL_C_INCLUDES += ./include ./src 
