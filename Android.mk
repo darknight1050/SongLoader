6280be3d146a3eaf8e76b440454b34f70257502d
darknight1050 <darkdeveloping1050@gmail.com>
2021-05-29 16:41:03 +0200

    Update to 1.16

--- Android.mk
+++ Android.mk
@@ -18,17 +18,17 @@ TARGET_ARCH_ABI := $(APP_ABI)
 
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: codegen - version: 0.8.1
+# Creating prebuilt for dependency: codegen - version: 0.9.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_8_1
+LOCAL_MODULE := codegen_0_9_0
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_8_1.so
+LOCAL_SRC_FILES := extern/libcodegen_0_9_0.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.3
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.5
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_3_3
+LOCAL_MODULE := beatsaber-hook_1_3_5
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_5.so
 LOCAL_CPP_FEATURES += exceptions 
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.1.0
@@ -43,7 +43,7 @@ LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.6.11
+# Creating prebuilt for dependency: questui - version: 0.7.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
@@ -64,8 +64,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/**,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_3
-LOCAL_SHARED_LIBRARIES += codegen_0_8_1
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_5
+LOCAL_SHARED_LIBRARIES += codegen_0_9_0
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_LDLIBS += -llog 
