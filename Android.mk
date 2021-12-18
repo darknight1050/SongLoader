7cdddbc22ad24f2c83ef248f4da2cb25d374493c
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2021-12-17 21:54:45 -0400

    Bump to 2.3.2

--- Android.mk
+++ Android.mk
@@ -18,11 +18,11 @@ TARGET_ARCH_ABI := $(APP_ABI)
 
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: beatsaber-hook - version: 2.3.0
+# Creating prebuilt for dependency: beatsaber-hook - version: 2.3.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_2_3_0
+LOCAL_MODULE := beatsaber-hook_2_3_2
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_3_0.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_3_2.so
 LOCAL_CPP_FEATURES += exceptions 
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.2.3
@@ -63,7 +63,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/**,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_2
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += codegen
