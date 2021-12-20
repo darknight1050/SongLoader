ec1cc3a7dba0b752834a6a7e10270cdfb676f43e
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-20 10:12:10 +0100

    Resolve Conflicts

--- build.ps1
+++ build.ps1
@@ -1,11 +1,7 @@
 Param(
     [Parameter(Mandatory=$false)]
-    [Switch]$clean,
-    [Parameter(Mandatory=$false)]
-    [Switch]$local_test,
-    [Parameter(Mandatory=$false)]
-    [Switch]$local_test_coroutine
-)
+    [Switch]$clean
+}
 
 # if user specified clean, remove all build files
 if ($clean.IsPresent)
@@ -24,14 +20,7 @@ if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
 }
 
 cd build
-if ($local_test.IsPresent) {
-    & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DLOCAL_TEST=a ../
-} elseif ($local_test_coroutine.IsPresent) {
-    & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DLOCAL_TEST_COROUTINE=a../
-} else {
-    & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" ../
-}
-
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" ../
 & cmake --build .
 $ExitCode = $LastExitCode
 cd ..
