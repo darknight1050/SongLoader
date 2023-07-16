40658e65e19cd89326f0d2217eb00ec84defb9ae
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-07-16 19:57:29 -0400

    Fix 2.1.0 parsing

--- src/LoadingFixHooks.cpp
+++ src/LoadingFixHooks.cpp
@@ -234,7 +234,47 @@ namespace RuntimeSongLoader::LoadingFixHooks {
 // Implementation by https://github.com/StackDoubleFlow
     MAKE_HOOK_MATCH(StandardLevelInfoSaveData_DeserializeFromJSONString, &GlobalNamespace::StandardLevelInfoSaveData::DeserializeFromJSONString, GlobalNamespace::StandardLevelInfoSaveData *, StringW stringData) {
         LOG_DEBUG("StandardLevelInfoSaveData_DeserializeFromJSONString");
-        auto *original = StandardLevelInfoSaveData_DeserializeFromJSONString(stringData);
+
+        GlobalNamespace::StandardLevelInfoSaveData *original;
+
+        // replacing the 2.x.x version with 2.0.0 is assumed safe because
+        // minor/patch versions should NOT break the schema and therefore deemed
+        // readable by RSL even if the new fields are not parsed
+        // short circuit
+        // 1.0.0 and 2.0.0 are supported by basegame through string equality checks
+        if (!stringData->Contains("1.0.0") && !stringData->Contains("2.0.0")) {
+            // https://regex101.com/r/jJAvvE/3
+            // Verified result: https://godbolt.org/z/MvfW3eh7q
+            // Checks if version is 2.0.0 range and then replaces it with 2.0.0
+            // for compatibility
+            static const std::regex versionRegex(
+                R"(\"version\"\s*:\s*\"(2\.\d\.\d)\")",
+                std::regex_constants::ECMAScript);
+
+            std::smatch matches;
+            std::string str(stringData);
+
+            if (!std::regex_search(str, matches, versionRegex)) {
+                // Does not match supported version
+                if (matches.empty() || matches.size() < 1) {
+                    return nullptr;
+                }
+
+                // match group is index 1 because we're matching for (2.x.x)
+                auto badVersion = matches[0].str();
+                // mutates the string does not copy
+                str.replace(matches[1].first, matches[1].second, "2.0.0");
+
+                original = StandardLevelInfoSaveData_DeserializeFromJSONString(
+                    str);
+            }
+        }
+
+        if (!original) {
+            original = StandardLevelInfoSaveData_DeserializeFromJSONString(
+                stringData);
+        }
+            
         if (!original)
             return nullptr;
 
