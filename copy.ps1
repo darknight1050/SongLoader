50406d928df84c3d0a3a234da2cae4273eff901f
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-01-25 16:39:35 +0100

    Project files bumped for unity update

--- copy.ps1
+++ copy.ps1
@@ -8,11 +8,13 @@ param (
 & ./build.ps1
 if($?) {
     if ($useDebug.IsPresent) {
-        & adb push build/debug/libsongloader.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libsongloader.so
+        & adb push build/debug/libsongloader.so /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/libsongloader.so
     } else {
-        & adb push build/libsongloader.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libsongloader.so
+        & adb push build/libsongloader.so /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/libsongloader.so
     }
     & adb shell am force-stop com.beatgames.beatsaber
+    & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+    & Start-Sleep -Seconds 1.0
     & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
     if ($log.IsPresent) {
         & ./log.ps1
